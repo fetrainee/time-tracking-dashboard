3cc7191124806cf25ed1f7510846c0ca47e7bbea
fetrainee <230843689+fetrainee@users.noreply.github.com>
2025-11-14 14:10:02 +0100

    Fix data loading

--- main.js
+++ main.js
@@ -51,7 +51,7 @@ const renderActivities = (activity) => {
 }
 
 
-fetch('/data.json')
+fetch('data.json')
     .then(response => {
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
